b34cb4df0786c52536d79ec203fe2c20ff573f06
Marcus Nyman <Marcusnyman556@gmail.com>
2020-11-25 18:52:10 +0100

    GDPR consent and priv-policy

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import {Route, BrowserRouter, Switch, Redirect} from 'react-router-dom';
 import { AuthTokenContext } from './context/Auth-token-context/AuthTokenContext'
 import LoginAndRegister from './components/login-and-register/login-and-register.component'
+import GDPRnotification from './components/GDPR-notification/GDPR-notification.component'
 
 import Money from './components/money/money.component'
 
 function App() {
   const [authToken] = useContext(AuthTokenContext);
+  const [gdprConsent, setGdprConsentToggle] = useState(true);
+
   return (
       <div className="App">
       <BrowserRouter>
         <Switch>
+        {gdprConsent ? <GDPRnotification setGdprConsentToggle={setGdprConsentToggle}/> : (
+          null
+        )}
         <Route exact path='/' component={LoginAndRegister} />
         <Route exact path="/money" render={() => {
         return (
@@ -23,6 +29,7 @@ function App() {
        />
         </Switch>
       </BrowserRouter>
+      
       </div>
   );
 }
